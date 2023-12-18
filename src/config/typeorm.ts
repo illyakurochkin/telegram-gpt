35a1fdde2fb55356199857c4e1d19ba0fa72e156
Illia Kurochkin <illya.kurochkin@gmail.com>
2023-12-18 21:33:48 +0100

    feat: delete

--- src/config/typeorm.ts
+++
@@ -1,9 +0,0 @@
-import type { TypeOrmModuleOptions } from '@nestjs/typeorm';
-
-export const databaseConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  url: process.env.DATABASE_URL,
-  ssl: false,
-  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  synchronize: true,
-};
