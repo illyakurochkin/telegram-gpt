cd181ef7b148791d42bcb86b6945cc150a1279f7
Illia Kurochkin <illya.kurochkin@gmail.com>
2023-12-23 08:50:33 +0100

    chore: add comments and improve code readability

--- src/modules/telegram/telegram.service.ts
+++ src/modules/telegram/telegram.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Context, Telegraf } from 'telegraf';
+import { Update } from '@telegraf/types';
 import { URL } from 'url';
 import { toMarkdown } from '../../utils/markdown';
 
@@ -11,6 +12,8 @@ export class TelegramService {
 
   /**
    * This method registers a command handler
+   * @param command - The command to register
+   * @param handler - The handler to register
    */
   public registerCommand(
     command: string,
@@ -22,6 +25,7 @@ export class TelegramService {
 
   /**
    * This method registers a message handler
+   * @param handler - The handler to register
    */
   public registerMessageHandler(handler: (ctx: Context) => Promise<void>) {
     this.telegraf.on('message', handler);
@@ -35,6 +39,73 @@ export class TelegramService {
     return this.telegraf.telegram.sendMessage(chatId, text);
   }
 
+  /**
+   * This method handles the start command
+   * Try to parse the text as markdown, if it fails, send as text
+   * @param chatId - The chat to send the message to
+   * @param text - The text to send (not parsed)
+   */
+  public async sendMarkdownMessage(chatId: number, text: string) {
+    const parsed = toMarkdown(text);
+
+    try {
+      return this.telegraf.telegram.sendMessage(chatId, parsed, {
+        parse_mode: 'MarkdownV2',
+      });
+    } catch {
+      this.logger.error("Couldn't parse markdown, sending as text", {
+        text,
+        parsed,
+      });
+      return this.telegraf.telegram.sendMessage(chatId, text);
+    }
+  }
+
+  /**
+   * This method handles the start command
+   * Try to parse the text as markdown, if it fails, send as text
+   * @param chatId - The chat to send the message to
+   * @param messageId - The message to edit
+   * @param text - The text to send (not parsed)
+   */
+  public async editMarkdownMessage(
+    chatId: number,
+    messageId: number,
+    text: string,
+  ) {
+    const parsed = toMarkdown(text);
+
+    try {
+      return this.telegraf.telegram.editMessageText(
+        chatId,
+        messageId,
+        undefined,
+        parsed,
+        {
+          parse_mode: 'MarkdownV2',
+        },
+      );
+    } catch {
+      this.logger.error("Couldn't parse markdown, sending as text", {
+        text,
+        parsed,
+      });
+      return this.telegraf.telegram.editMessageText(
+        chatId,
+        messageId,
+        undefined,
+        text,
+      );
+    }
+  }
+
+  /**
+   * This method sends async messages
+   * It sends a loading message first, then edits it with the result of the promise
+   * @param chatId - The chat to send the message to
+   * @param loadingText - The text to send while loading
+   * @param promise - The promise to await
+   */
   public async sendAsyncMessage(
     chatId: number,
     loadingText: string,
@@ -46,37 +117,30 @@ export class TelegramService {
     );
 
     const text = await promise;
-    const parsed = toMarkdown(text);
-
-    try {
-      await this.telegraf.telegram.editMessageText(
-        chatId,
-        loadingMessage.message_id,
-        undefined,
-        parsed,
-        {
-          parse_mode: 'MarkdownV2',
-        },
-      );
-    } catch {
-      await this.telegraf.telegram.editMessageText(
-        chatId,
-        loadingMessage.message_id,
-        undefined,
-        text,
-      );
-    }
+    await this.editMarkdownMessage(chatId, loadingMessage.message_id, text);
   }
 
   /**
    * This method handles the start command
+   * @param chatId - The chat to delete the message from
+   * @param messageId - The message to delete
    */
   public async deleteMessage(chatId: number, messageId: number) {
     return this.telegraf.telegram.deleteMessage(chatId, messageId);
   }
 
   /**
+   * This method handles telegram updates (new messages, etc)
+   * It's only used for the webhook
+   * @param update
+   */
+  public async handleUpdate(update: Update) {
+    return this.telegraf.handleUpdate(update);
+  }
+
+  /**
    * This method sets the webhook for the telegram bot
+   * @param webhookUrl - The url to set the webhook to
    */
   public async launchWebhook(webhookUrl: string) {
     const { hostname, port, pathname } = new URL(webhookUrl);
